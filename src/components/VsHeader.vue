770e5b61cedf76b7077f56468f120efedd6319ab
Dylan Davies <dylandaviddavies@gmail.com>
2020-07-02 22:48:29 -0400

    Made avatar link send you to dylandavies.me

--- src/components/VsHeader.vue
+++ src/components/VsHeader.vue
@@ -1,10 +1,15 @@
 <template>
   <header class="vs-header">
-    <router-link class="vs-header__avatar" to="/account">
+    <a
+      class="vs-header__avatar"
+      href="https://dylandavies.me/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img
         src="https://media-exp1.licdn.com/dms/image/C5603AQH3IdNkmuru3w/profile-displayphoto-shrink_200_200/0?e=1597276800&v=beta&t=nWf3DN2mAnuADI_NxN0iNG4l7X8E-gmewouJC-cOCqI"
       />
-    </router-link>
+    </a>
   </header>
 </template>
 
